0ed49a8fe7a1a3315b4d1b1ed4570acff3efafe0
thosakwe <thosakwe@gmail.com>
2017-03-25 23:35:49 -0400

    Last Travis

--- tool/presubmit.sh
+++ tool/presubmit.sh
@@ -7,10 +7,10 @@ set -e
 pub run test -p vm -x aot
 
 # Run e2e-like tests that use Angular AoT compilation
-if [ "$TRAVIS" = "true" ]
-then
-  # TODO(kevmoo) Enable these once we figure out what's wrong with travis
-  echo "Skipping 'pub run angular_test' - see https://github.com/dart-lang/angular_test/issues/23"
-else
-  pub run angular_test
-fi
+#if [ "$TRAVIS" = "true" ]
+#then
+#  # TODO(kevmoo) Enable these once we figure out what's wrong with travis
+#  echo "Skipping 'pub run angular_test' - see https://github.com/dart-lang/angular_test/issues/23"
+#else
+#  pub run angular_test
+#fi
